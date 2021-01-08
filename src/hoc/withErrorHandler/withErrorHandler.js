f8171759083111db5640e6fef7cadf4273cc670d
Ricardo Proença <ricproenca@gmail.com>
2021-01-08 12:50:55 +0100

    Removing Old Interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,15 +10,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
         };
 
         componentWillMount() {
-            axios.interceptors.request.use(request => {
+            this.reqInterceptor = axios.interceptors.request.use(request => {
                 this.setState({ error: null });
                 return request;
             });
 
-            axios.interceptors.response.use(
+            this.resInterceptor = axios.interceptors.response.use(
                 response => response,
                 error => this.setState({ error: error })
             );
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
